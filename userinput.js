31202d81145f9fd167f78e76c16f8167b7803780
Fazal2307-dev <mdfazal2307@gmail.com>
2025-08-17 19:51:54 +0530

    taking input from user

---
+++ userinput.js
@@ -0,0 +1,10 @@
+console.log("hey Md ");
+
+const rdf = require('readline-sync');
+
+// const userName =  readlineSync.question('May I know your name? ');
+// console.log(`Welcome, ${userName}!`);
+
+const age = rdf.question('whats your age! ');
+console.log(`your age is ${age}`);
+// scanf("%d",&a);
