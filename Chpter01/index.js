7072de8df1fb6b7750036e10060fbed029609b9b
Fazal2307-dev <mdfazal2307@gmail.com>
2025-09-01 18:06:09 +0530

    js var functional scope hai

--- Chpter01/index.js
+++ Chpter01/index.js
@@ -1,3 +1,6 @@
 console.log("Hello fazal");
 // var,let,const
 //scope(global,block,functional)
+//scope means knha tak uska dayra hai
+// var functional scoped hai other wise wo janha chahe wanha use ho skta hai
+// 
