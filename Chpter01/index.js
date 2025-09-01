5a3f2dfed93ccfd4e0680214f7e1ed5a45366612
Fazal2307-dev <mdfazal2307@gmail.com>
2025-09-01 18:11:03 +0530

    js engine

--- Chpter01/index.js
+++ Chpter01/index.js
@@ -3,4 +3,6 @@ console.log("Hello fazal");
 //scope(global,block,functional)
 //scope means knha tak uska dayra hai
 // var functional scoped hai other wise wo janha chahe wanha use ho skta hai
+// agar tumne var ko functional mein nhi dala hai aur usko print 
+// kara rahe ho to js engine isko functional scope mein daalega aur tab value return karega
 // 
