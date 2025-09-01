ebf496cc42b6e6d1dee2bcc58cec261a63d5fb2b
Fazal2307-dev <mdfazal2307@gmail.com>
2025-09-01 17:33:21 +0530

    js

--- swapTwoNumber.js
+++
@@ -1,8 +0,0 @@
-let readlineSync = require("readline-sync");
-let a = Number(readlineSync.question('please Enter Value of a? '));
-let b = Number(readlineSync.question('please Enter Value of b? '));
-a = a+b;
-b = a-b;
-a = a-b;
-console.log(a);
-console.log(b);
