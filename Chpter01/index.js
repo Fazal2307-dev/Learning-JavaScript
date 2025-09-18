62cb14879ce7670f2e1be39caba7ba5b653d4f5e
Fazal2307-dev <mdfazal2307@gmail.com>
2025-09-18 10:36:13 +0530

    word and keyword

--- Chpter01/index.js
+++ Chpter01/index.js
@@ -1,8 +1,4 @@
-console.log("Hello fazal");
-// var,let,const
-//scope(global,block,functional)
-//scope means knha tak uska dayra hai
-// var functional scoped hai other wise wo janha chahe wanha use ho skta hai
-// agar tumne var ko functional mein nhi dala hai aur usko print 
-// kara rahe ho to js engine isko functional scope mein daalega aur tab value return karega
-// 
+// word vs keyword
+//var let const-keywords
+//js mein aaisa words jinse kuch ho skta hai
+//word-lolo,fazal,name,
