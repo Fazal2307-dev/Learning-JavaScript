e9f5d1500c29907a191bf0288c4fd8646ee65c19
Fazal2307-dev <mdfazal2307@gmail.com>
2025-09-18 10:46:58 +0530

    var use

--- Chpter01/index.js
+++ Chpter01/index.js
@@ -3,4 +3,8 @@
 //js mein aaisa words jinse kuch ho skta hai
 //word-lolo,fazal,name,
 // use let ,const
-//don't use var ,var is old
+//don't use var ,var is 
+//var-ES5 ka part hai
+//var ka use poore programm mmein use ho skta hai 
+// just like atm machine agar vo bhul jaye kon ho tum to har bar vo puche ga kon ho tum 
+//isliye var ka use karte hain for whole programm.
