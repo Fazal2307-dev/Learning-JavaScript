e3e7cfc8dd9597c3196efe14168f2ef7415d69ee
Fazal2307-dev <mdfazal2307@gmail.com>
2025-09-24 21:33:36 +0530

    changing value throuh primitive or refrenece

--- Chapter02/index.js
+++ Chapter02/index.js
@@ -2,10 +2,15 @@
 data types:-1.primitive,2.Reference
 1.Primitive->aisi saree value jinko copy karne par tumhe ek real copy  mil jaaye
 string,number,boolean,null,undefined,symbol,bigint are primitive value;
+let  a=12;
+let b=a;
+ a= a+2;
+ to ab a ki value 14 hogi aur b ki abhi bhi 12 hai
+
 
 2.Refrence->inko copy karne par real copy nahi milegi but aapko refrence milega parent ka 
 (array,object ,function are refernce value )
-
+ 
 
 
 
