48e6916486aaec8dea604473d861985dbd3082d1
Fazal2307-dev <mdfazal2307@gmail.com>
2025-09-30 20:06:09 +0530

    assignment operator

--- Chapter03/index.js
+++ Chapter03/index.js
@@ -5,13 +5,26 @@ arithmetic,cimparison,logical,assignment,unary,ternary
 **->exponantiation
 2**3=8
 
-== ->12==13//false
+== ->non strict compar
+12==13//false
 12=="12"//true isliye ke vo sirf value ko dekh raha hai type ko nhi
 12==="12"//false ab ye value and type dono ko chek kar raha hai
 
-===
-!=
-!==
+===->strict comp
+!=non strict comp
+
+12!=13//true
+
+!==->strict comparison
+12!=="12"//true
+
++=
+-=
+*=
+let a=12;
+a+=12;
+means ,a=a+12;
+
 
 
 
