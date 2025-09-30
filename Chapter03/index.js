73920f62cca1102ad51e4c0ea4e9442437e6327f
Fazal2307-dev <mdfazal2307@gmail.com>
2025-09-30 19:58:49 +0530

    ==,=== vaalue and type

--- Chapter03/index.js
+++ Chapter03/index.js
@@ -5,6 +5,14 @@ arithmetic,cimparison,logical,assignment,unary,ternary
 **->exponantiation
 2**3=8
 
+== ->12==13//false
+12=="12"//true isliye ke vo sirf value ko dekh raha hai type ko nhi
+12==="12"//false ab ye value and type dono ko chek kar raha hai
+
+===
+!=
+!==
+
 
 
 
