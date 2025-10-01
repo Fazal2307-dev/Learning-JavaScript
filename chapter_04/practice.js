6755e71455f7c3f85a9d7f22c7ea3d39faa5540d
Fazal2307-dev <mdfazal2307@gmail.com>
2025-10-01 15:45:36 +0530

    using function early return

--- chapter_04/practice.js
+++ chapter_04/practice.js
@@ -14,3 +14,12 @@
 //         break;
 //     default:
 // }
+
+function getVal(val){
+    if(val<100) return "A";
+    else if(val<75) return "B";
+    else if(val<65) return "C";
+    else return "D";
+
+}
+console.log(getVal(76));
