a8d88e3b593425b534d5c278d24a34482aea4d4a
Fazal2307-dev <mdfazal2307@gmail.com>
2025-10-01 16:12:48 +0530

    rock,papper,scissor

--- Chapter04/practice.js
+++ Chapter04/practice.js
@@ -24,15 +24,28 @@
 // }
 // console.log(getVal(76));
 
-function getGrade(score){
-if(score>=90 && score<=100) return "A+";
-else if(score>=80 && score<=89) return "A";
-else if(score>=70 && score<=79) return "B";
-else if(score>=60 && score<=69) return "C";
-else if(score>=33 && score<=59) return "D";
-else if(score>=0 && score<=32) return "E";
-else return "Invalid marks";
+// function getGrade(score){
+// if(score>=90 && score<=100) return "A+";
+// else if(score>=80 && score<=89) return "A";
+// else if(score>=70 && score<=79) return "B";
+// else if(score>=60 && score<=69) return "C";
+// else if(score>=33 && score<=59) return "D";
+// else if(score>=0 && score<=32) return "E";
+// else return "Invalid marks";
 
 
+// }
+// console.log(getGrade(40));
+
+//rock papper scissor
+function rps(user,comp){
+    if(user === comp) return "Draw";
+    if(user === "rock" && comp==="scissor") return "user";
+    if(user==="scissor" && comp==="papper") return "user";
+    if(user==="papper" && comp==="rock") return "user";
+
+    return "computer";
 }
-console.log(getGrade(40));
+
+
+console.log(rps("rock","paper"));
