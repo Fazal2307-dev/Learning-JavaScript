eabad5ea1718bf629ce574531fc01967891af60a
Fazal2307-dev <mdfazal2307@gmail.com>
2025-10-01 16:59:35 +0530

    while()

--- Chapter04/index.js
+++ Chapter04/index.js
@@ -22,17 +22,36 @@ swtich(val){
         console.log("Val 3");
         break;
     default:
-
+}
 repeat karne ko loop kahte hain
 for,while,foreach
 
+kaha se jana hai->kaha tak jana hai-> kaise jana hai
+for
+1-45->for
+20-30->for
+for(start,end,change){}
 
 
+kaha se jana hai->kab rukna hai->kaise jana hai
+while()
+hello na aajaye->while()
+tan tak likho jab tak ink khatam na ho jaye->while()
 
-
-
-
+start;
+while(end){
+change;
 }
+
+
+
+
+
+
+
+
+
+
 
 
 
