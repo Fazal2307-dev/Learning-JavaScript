926c20c479cbca3b0146845f2e6fae633c36242b
Fazal2307-dev <mdfazal2307@gmail.com>
2025-10-01 15:41:34 +0530

    practice js

---
+++ chapter_04/practice.js
@@ -0,0 +1,16 @@
+
+
+// let val = 2;
+
+// Swtich(2) {
+//     case 1:
+//         console.log("Val 1");
+//         break;
+//     case 2:
+//         console.log("Val 2");
+//         break;
+//     case 3:
+//         console.log("Val 3");
+//         break;
+//     default:
+// }
