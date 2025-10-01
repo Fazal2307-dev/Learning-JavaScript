05225dc0170a20b7b3cce1a726885389416bc101
Fazal2307-dev <mdfazal2307@gmail.com>
2025-10-01 15:41:17 +0530

    index.js

--- chapter_04/index.js
+++ chapter_04/index.js
@@ -3,6 +3,35 @@ if,elsse,else if
 switch case
 early return pattern
 
+if(valid){
+
+}else if(!valid){
+
+}else
+
+we can use unlimited else if 
+
+swtich(val){
+    case 1:
+        console.log("Val 1");
+        break;
+    case 2:
+        console.log("Val 2");
+        break;
+    case 3:
+        console.log("Val 3");
+        break;
+    default:
+
+
+
+
+
+
+
+}
+
+
 
 
 
