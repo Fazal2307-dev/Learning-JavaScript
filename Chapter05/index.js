8baa943160b9cd06ec471b3dd379a4991987b5c6
Fazal2307-dev <mdfazal2307@gmail.com>
2025-10-03 21:52:27 +0530

    function

--- Chapter05/index.js
+++ Chapter05/index.js
@@ -1,4 +1,6 @@
 /* funtion---------------------------------------------------------------------------------------------------------------
+what ,why,how
+function kisi bhi naam se suru kar skte hain sirf number,symbol,keyword se nhi kar skte
 
 
 
