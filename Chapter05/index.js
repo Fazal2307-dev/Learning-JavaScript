70e39119f05a8a9263f5035ae102d922a75761c1
Fazal2307-dev <mdfazal2307@gmail.com>
2025-10-03 22:00:18 +0530

    arrow function

--- Chapter05/index.js
+++ Chapter05/index.js
@@ -2,17 +2,24 @@
 what ,why,how
 function kisi bhi naam se suru kar skte hain sirf number,symbol,keyword se nhi kar skte
 1. function happy(){
+//function decleration
 
 console.log("Happy Birth Day")
 }
 happy();
 2. let fun = function(){
+//function expression
 console.log("Happy Birthday fazal")
 }
 fun();
 ye dono trah se function likh skte hain
 
-
+// fat arrow fnc
+()=>{}
+let happy = ()=>{
+    
+    
+    }
 
 
 
