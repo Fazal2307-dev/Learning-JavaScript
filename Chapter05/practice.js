5fc75e5ab2c6c7e417e26007cba433c340c7739b
Fazal2307-dev <mdfazal2307@gmail.com>
2025-10-03 23:01:34 +0530

    using n+default parameter

--- Chapter05/practice.js
+++ Chapter05/practice.js
@@ -1,18 +1,24 @@
-let happy = ()=>{
-    console.log("Fazal");
-}
-happy();
+// let happy = ()=>{
+//     console.log("Fazal");
+// }
+// happy();
 
-function sum(v1,v2){
-    console.log(v1 + v2);
-}
-sum(1,2);
+// function sum(v1,v2){
+//     console.log(v1 + v2);
+// }
+// sum(1,2);
 
-function  dance(v){
-    console.log(`${v} naach raha hai`)
+// function  dance(v){
+//     console.log(`${v} naach raha hai`)
 
+// }
+// dance("Fazal");
+// dance("Afzal");
+// dance("AAdmi");
+// dance("Life");
+
+function add(v1=0,v2=0){
+
+    console.log(v1+v2);
 }
-dance("Fazal");
-dance("Afzal");
-dance("AAdmi");
-dance("Life");
+add(12,32.5648);
