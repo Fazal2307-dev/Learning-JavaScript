85a49744ffab7d245ba910ea4d2329b6ce39bf3e
Fazal2307-dev <mdfazal2307@gmail.com>
2025-10-03 22:10:17 +0530

    using backtick for dynamic value

--- Chapter05/practice.js
+++ Chapter05/practice.js
@@ -2,3 +2,17 @@ let happy = ()=>{
     console.log("Fazal");
 }
 happy();
+
+function sum(v1,v2){
+    console.log(v1 + v2);
+}
+sum(1,2);
+
+function  dance(v){
+    console.log(`${v} naach raha hai`)
+
+}
+dance("Fazal");
+dance("Afzal");
+dance("AAdmi");
+dance("Life");
