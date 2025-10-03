a92610471a1c202a5bd8780e618bef8c383c02e0
Fazal2307-dev <mdfazal2307@gmail.com>
2025-10-03 21:54:41 +0530

    wrting way of fnc

--- Chapter05/index.js
+++ Chapter05/index.js
@@ -1,7 +1,16 @@
 /* funtion---------------------------------------------------------------------------------------------------------------
 what ,why,how
 function kisi bhi naam se suru kar skte hain sirf number,symbol,keyword se nhi kar skte
+1. function happy(){
 
+console.log("Happy Birth Day")
+}
+happy();
+2. let fun = function(){
+console.log("Happy Birthday fazal")
+}
+fun();
+ye dono trah se function likh skte hain
 
 
 
