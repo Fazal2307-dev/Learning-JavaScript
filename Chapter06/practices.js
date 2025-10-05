3ee6c67d84578f06207350cf665dfe43a5f69fec
Fazal2307-dev <mdfazal2307@gmail.com>
2025-10-05 11:34:28 +0530

    counter function

--- Chapter06/practices.js
+++ Chapter06/practices.js
@@ -1,11 +1,23 @@
 //P2 create a reusable discount calculator(hof)
-function calculator(discountValue){
-    return function(price){
-        return price - price *(discountValue / 100);
-    }
-}
+// function calculator(discountValue){
+//     return function(price){
+//         return price - price *(discountValue / 100);
+//     }
+// }
 
-let discount = calculator(10)
-console.log(discount(200));
-let twentyDiscount = calculator(20);
-console.log(twentyDiscount(1500));
+// let discount = calculator(10)
+// console.log(discount(200));
+// let twentyDiscount = calculator(20);
+// console.log(twentyDiscount(1500));
+
+//P3
+function counter(){
+    let count =0;
+    return function(){
+        count++;
+        return count;
+    }
+
+}
+let c = counter()
+console.log(c())
