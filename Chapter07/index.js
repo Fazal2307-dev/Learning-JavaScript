c909cb903a4c7bbaab7b9054aaa077b6e217120a
Fazal2307-dev <mdfazal2307@gmail.com>
2025-10-07 11:29:56 +0530

    destructuring in js

--- Chapter07/index.js
+++ Chapter07/index.js
@@ -8,11 +8,40 @@ eduction:"B.Tech(cse)",
 college:"LPU",
 };
 console.log(obj.name);
-console.log(obj[age]);
+console.log(obj['age']);
+key value pair->
+let obj = {
+name:"FAZAL"
+}
+name - key,
+"Fazal"-value
 
+dot vs bracket 
+ let obj ={
+ name:"FAZAL",
+ age:23,
+ }
+ obj.name;'fazal'
+ obj['name];'fazal
+ both give you answer
 
+ nesting and deep access->----------------------------------------
 
+const user = {
+name:"FAZAL",
+Address:{
+city:"Patna",
+Pincode:803213,
+location:{
+lat:22.5,
+lang:12.87,
+     }
+  }
+}
 
+console.log(user.Address.location.lang)
+//destructure
+let {lang,lat}=user.address.location
 
 
 
--- Chapter07/practices.js
+++ Chapter07/practices.js
@@ -1,9 +1,25 @@
-let obj = {
-name:"Fazal",
-age:24,
-email:"mdfazal@gmail.com",
-eduction:"B.Tech(cse)",
-college: "LPU",
-}
-console.log(obj.name);
-console.log(obj['age']);
+// let obj = {
+// name:"Fazal",
+// age:24,
+// email:"mdfazal@gmail.com",
+// eduction:"B.Tech(cse)",
+// college: "LPU",
+// }
+// console.log(obj.name);
+// console.log(obj['age']);
+
+const user = {
+name:"FAZAL",
+Address:{
+city:"Patna",
+Pincode:803213,
+location:{
+lat:22.5,
+lang:12.87,
+     },
+  },
+};
+
+// console.log(user.Address.location.lang)
+let {lang,lat}=user.Address.location
+console.log(lang);
