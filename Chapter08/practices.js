3817c62a06c1594d79aaa0b0eaaff850ec30bdfb
Fazal2307-dev <mdfazal2307@gmail.com>
2025-10-08 20:44:20 +0530

    removeAttribute

--- Chapter08/practices.js
+++ Chapter08/practices.js
@@ -13,5 +13,6 @@ let a = document.querySelector("a");
 // a.setAttribute("href","https://www.google.com");
 // console.dir(a);
 
-console.log(a.getAttribute("href"));
+// console.log(a.getAttribute("href"));
+console.log(a.removeAttribute("href"));
 
