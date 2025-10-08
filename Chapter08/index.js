88f88798e77895e7ddeaec24a748d65dbaf400af
Fazal2307-dev <mdfazal2307@gmail.com>
2025-10-08 19:57:30 +0530

    innerHTML

--- Chapter08/index.js
+++ Chapter08/index.js
@@ -8,13 +8,18 @@ css badlna
 attribute
 event listeners
 
-for selecting  html element there is four way 
+for selecting  html elements there is four way 
 
 1.document.querySelector()
 2.document.querySelectorAll()
 3.document.getElementById()
 4.document.getElementsByClassName();
 
+agar same name element hai like html do to queryselector first wale ko select karega
+manipulation
+h1.innerHtml()
+.innerText()
+.textContent()
 
 
 
--- Chapter08/practices.js
+++ Chapter08/practices.js
@@ -2,5 +2,9 @@
 // console.dir(abcd);
 // let abcd1 = document.getElementsByClassName("abcd1");
 // console.dir(abcd1);
-let abcd = document.querySelectorAll("h1");
-console.dir(abcd)
+// let abcd = document.querySelectorAll("h1");
+// console.dir(abcd)
+
+let h1 = document.querySelector("h1");
+h1.textContent=("hi fazal waht r you doing")
+
