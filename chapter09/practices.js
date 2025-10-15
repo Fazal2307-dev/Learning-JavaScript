e0f3c0a1c8c7a7f0f09f7e19c62db3ef62885bc1
Fazal2307-dev <mdfazal2307@gmail.com>
2025-10-15 11:03:08 +0530

    dynamic data flow through function

--- chapter09/practices.js
+++ chapter09/practices.js
@@ -2,3 +2,26 @@ let h1 = document.querySelector("h1");
 h1.addEventListener("click",function(){
    h1.style.color="red";
 })
+
+// let p = document.querySelector("p");
+// p.addEventListener("click",function(){
+//     p.style.color="green";
+// })
+
+// let p = document.querySelector("p");
+// function dobclick(){
+//     p.style.color="red";
+// }
+// p.addEventListener("dblclick",dobclick);
+// p.removeEventListener("dblclick",dobclick)
+
+// let inpt = document.querySelector("input");
+// inpt.addEventListener('input',function(e){
+//     console.log(e);
+    
+// })
+let inpt = document.querySelector("input");
+inpt.addEventListener("input",function(e){
+    if(e.data !== null){   console.log(e.data);}
+      
+})
