5237f869df6550fd085221cc16259139c04a1c60
Fazal2307-dev <mdfazal2307@gmail.com>
2025-10-15 11:03:00 +0530

    dynamic data flow through function

--- chapter09/index.js
+++ chapter09/index.js
@@ -7,4 +7,16 @@ let h1 = document.querySelector("h1");
 h1.addEventListener("click",function(){
    h1.style.color="red";
 })
+   1.select the tag or element first for using querySelector than
+   use addEventListener, addEventListener use two thing first ("click",function(){
+   then use style.color=""})
+
+if we want to use .removeEventListener to hame addEventListener ke fnction ko bahar bna padega
+jaise 
+let p= document.addEventListener("p");
+function dobclick(){
+p.style.color="red";
+}
+p.addEventListener("dblclick",dobclick);
+is trah se use karte hai aur chahe to .removeEventListener laga skte hain
 */
