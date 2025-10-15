c4d3369c9aebfd8bf4f61b763b3e97da8bba472a
Fazal2307-dev <mdfazal2307@gmail.com>
2025-10-15 09:56:28 +0530

    chapter09

--- React_JS_Theory/practices.js
+++ React_JS_Theory/practices.js
@@ -1,10 +1,25 @@
+export const username = "Fazal";
+
 const a = 20;
 export default a;
 
 
 
 /*
-import means koi bhi chiz import karna a
-export means kisi bhi chiz ko export karna
-default means mai sirf ek value bhej raha hun
+.1import means koi bhi chiz import karna a
+.2export means kisi bhi chiz ko export karna
+.3default means mai sirf ek value bhej raha hun
+.4if export along with varible hai to vo named export  kahlata hai 
+  aur vo access import {name}isi trah se hota hai.
+.5Real dom vs virtual dom-> real dom means structure of html like node structure ab maan lo mere html m bahut sare tage hai main 
+chaahta hun ke sirf ek element change ho pura na ho islye virtual dom laya gya virtual dom means copy of real dom 
+agar koi chnage aap kar rahe hain to pahle virtual dom  mein hoga phir vo compare karega real dom se ussi element ko change karega 
+na ki pure html ko aur pure ko rerender nhi karega isse web site smooth kaam karega aur aapka server cost bhi kam hoga 
+1. User triggers change (e.g. button click)
+2. Virtual DOM updates
+3. Diffing algorithm finds minimal changes
+4. Real DOM updates only that part
+5. UI stays fast and responsive
+.6jsx means we can write html code in js like this
+const v1 = <h1>hello world!</h1>
 */
