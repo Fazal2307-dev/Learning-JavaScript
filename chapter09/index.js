83ef4418de73f1df5af82d88e1d1aea98c45e48e
Fazal2307-dev <mdfazal2307@gmail.com>
2025-10-15 10:09:37 +0530

    addEventListener

---
+++ chapter09/index.js
@@ -0,0 +1,10 @@
+/*
+browser mein page par koi harkat karo event raise ho jayega 
+event matlab hota hai koi action hua 
+event listener ka matlab hai ke aapne koi action ka reaction diya 
+
+let h1 = document.querySelector("h1");
+h1.addEventListener("click",function(){
+   h1.style.color="red";
+})
+*/
