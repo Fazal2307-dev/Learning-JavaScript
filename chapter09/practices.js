5cbcdbf46e4dc60d0a07956e3dc285fe89b4db7d
Fazal2307-dev <mdfazal2307@gmail.com>
2025-10-16 10:16:52 +0530

    changing value through window keyword

--- chapter09/practices.js
+++ chapter09/practices.js
@@ -27,8 +27,17 @@
 // })
 
 //on change
-let sel = document.querySelector("select");
-let device = document.querySelector("#device");
-sel.addEventListener("change",function(dets){
-    device.textContent= `You Selected ${dets.target.value}`;
+// let sel = document.querySelector("select");
+// let device = document.querySelector("#device");
+// sel.addEventListener("change",function(dets){
+//     device.textContent= `You Selected ${dets.target.value}`;
+// })
+//window
+let h1 = document.querySelector("h1");
+window.addEventListener("keydown",function(dets){
+if(dets.key === " "){
+    h1.textContent= "You Typed SPACE"
+}else{
+    h1.textContent = dets.key;
+}
 })
